e31f298d18b8e55aff80c47b69a7df9371c7cfa4
esoni <dario.frongillo@gmail.com>
2020-05-14 12:45:02 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t dariofrongillo/multi-client:latest -t dariofrongillo/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t dariofrongillo/multi-server:latest -t dariofrongillo/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t dariofrongillo/multi-worker:latest -t dariofrongillo/multi-worker:$SHA  -f ./worker/Dockerfile ./worker
+docker build -t dariofrongillo/multi-worker:latest -t dariofrongillo/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push dariofrongillo/multi-client:$SHA
 docker push dariofrongillo/multi-server:$SHA
 docker push dariofrongillo/multi-worker:$SHA
